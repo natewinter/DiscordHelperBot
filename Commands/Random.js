f2d7b002758ef660e0fb18886c2b0e4f983db8b7
Josh Brown <joshbrown2794@gmail.com>
2020-12-17 14:15:51 -0800

    omdb can console log but not send message due to promise

--- Commands/Random.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "Random",
-  description: "Random ",
-  execute(message, args) {
-    message.channel.send(
-      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
-    );
-  },
-};
