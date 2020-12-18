9bf1dff7646a8bac3c5094cfb9a39d86ebf59d17
Josh Brown <joshbrown2794@gmail.com>
2020-12-18 12:25:11 -0800

    process.env.key

--- Commands/Movie.js
+++ Commands/Movie.js
@@ -4,12 +4,9 @@ module.exports = {
   description: "search for a movie!",
   async execute(message, args) {
     const search = args.join("+");
-    // // let url = `http://www.omdbapi.com/?apikey=${process.env.omdbKey}&t=star+wars`;
-    // let url = `http://www.omdbapi.com/?i=tt3896198&apikey=452f6357`;
-    // //www.omdbapi.com/?i=tt3896198&apikey=452f6357
     let getMovie = async () => {
       let result = await fetch(
-        `http://www.omdbapi.com/?t=${search}&plot=full&apikey=452f6357`
+        `http://www.omdbapi.com/?t=${search}&plot=full&apikey=${process.env.omdbKey}`
       );
       let json = await result.json();
       return json;
@@ -32,4 +29,4 @@ module.exports = {
   },
 };
 
-// can get json data of movie but no message sent back.
+// need to look into how to turn this into card so it isnt a message.
