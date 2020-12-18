568f7a3c98ae08aae40da0efc9c629a1554378b5
Josh Brown <joshbrown2794@gmail.com>
2020-12-18 12:16:53 -0800

    search args working for movie command

--- Commands/Movie.js
+++ Commands/Movie.js
@@ -3,36 +3,13 @@ module.exports = {
   name: "movie",
   description: "search for a movie!",
   async execute(message, args) {
-    // const search = args.join("+");
+    const search = args.join("+");
     // // let url = `http://www.omdbapi.com/?apikey=${process.env.omdbKey}&t=star+wars`;
     // let url = `http://www.omdbapi.com/?i=tt3896198&apikey=452f6357`;
     // //www.omdbapi.com/?i=tt3896198&apikey=452f6357
-    // http: axios
-    //   .get(url)
-    //   .then(function (response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   })
-    //   .then(function (response) {
-    //     message.channel.send(response);
-    //   });
-    // try {
-    //   const response = await axios.get(
-    //     "http://www.omdbapi.com/?i=tt3896198&apikey=452f6357"
-    //   );
-    //   let data = response.data;
-    //   console.log(data);
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    // then(function () {
-    //   message.channel.send(data);
-    // });
     let getMovie = async () => {
       let result = await fetch(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=452f6357"
+        `http://www.omdbapi.com/?t=${search}&apikey=452f6357`
       );
       let json = await result.json();
       return json;
